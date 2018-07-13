c7cd79de23cd783b35c67668ab8af389eacd344c
Алексей <alexyatsevich@ymail.com>
2018-07-13 20:39:21 +0300

    Initial Commit

---
+++ ChildNavController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChildNavController
+//
+//  Created by Алексей Яцевич on 7/13/18.
+//  Copyright © 2018 Алексей Яцевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
